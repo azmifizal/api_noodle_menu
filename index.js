cf71a796ba22392e20a441c510ea41ac765bc50b
azmifizal <azmifizalfauzi@gmail.com>
2020-06-14 17:51:19 +0800

    add cors middleware

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect(
   "mongodb://userNoodle:noodle123@ds157509.mlab.com:57509/portofolio",
